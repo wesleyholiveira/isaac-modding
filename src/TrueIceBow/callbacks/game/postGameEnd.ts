4bf7bbb6c6e37ce8cf0f0b5a84fe82d7dfa28fcc
Wesley Oliveira <wesleyfetish@gmail.com>
2023-02-06 11:59:07 -0300

    fix: removido callback não utilizado de post_game_end

--- src/TrueIceBow/callbacks/game/postGameEnd.ts
+++
@@ -1,7 +0,0 @@
-import { ModCallback } from "isaac-typescript-definitions";
-
-export function postGameEnd(mod: Mod) {
-  mod.AddCallback(ModCallback.POST_GAME_END, main);
-}
-
-function main(isGameOver: boolean) {}
